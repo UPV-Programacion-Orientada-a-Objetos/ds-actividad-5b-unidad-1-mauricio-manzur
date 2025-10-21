9525070244bf89684abb910e29692de203faea32
Manzur <rfarfa965@gmail.com>
2025-10-20 19:44:34 -0600

    Se agregó la posibilidad de poder realizar la suma de tanto matrices estaticas como dinamicas, tambien se cambiaron algunas instrucciones de codigo en el archivo .h por que no estaban bien implementadas.

--- clases.h
+++ clases.h
@@ -1,129 +1,150 @@
+#include <iostream>
 
 template <typename T>
 class MatrizBase {
+
 protected:
     int _filas;
     int _columnas;
 
 public:
+    // Constructor
+    MatrizBase() : _filas(5), _columnas(5) {}
+    
+    // Método para obtener elemento (necesario para suma)
+    virtual T obtenerElemento(int i, int j) const = 0;
+    
     virtual void cargarValores() = 0;
     virtual MatrizBase<T>* sumar(const MatrizBase<T>& otra) const = 0;
     virtual void imprimir() const = 0;
+    
+    // Destructor virtual
+    virtual ~MatrizBase() = default;
 };
 
 template <typename T>
 class MatrizDinamica : public MatrizBase<T> {
 
-
+private:
     T **_datos;
 
+public:
+    // Constructor
+    MatrizDinamica() {
+        this->_filas = 5;
+        this->_columnas = 5;
+        _datos = nullptr;
+    }
+
+    // Método para obtener elemento
+    T obtenerElemento(int i, int j) const override {
+        return _datos[i][j];
+    }
+
     // sobreescribo los valores de la clase MatrizBase: cargarValores.
-    void cargarValores() override{
-
+    void cargarValores() override {
+        _datos = new T*[this->_filas];
         
+        int contador = 1; // este valor solamente representa la cantidad que se le asignara a la posicion [i][j]
+        
+        for(int i = 0; i < this->_filas; i++){
+            _datos[i] = new T[this->_columnas];
+        }
 
+        for(int i = 0; i < this->_filas; i++){
+            for(int j = 0; j < this->_columnas; j++){
+                _datos[i][j] = contador*2;
+                contador++;
+            }
+        }
     }
 
     // sobreescribo los valores de la clase MatrizBase: imprimir.
-    void imprimir() override{
-
-        for(int i = 0; i < _filas; i++){
-
-            for(int j = 0; j < _columnas; j++){
-
-                std::cout << "******** Matriz Dinamica *********" << std::endl;
+    void imprimir() const override {
+        std::cout << "******** Matriz Dinamica *********" << std::endl;
+        for(int i = 0; i < this->_filas; i++){
+            for(int j = 0; j < this->_columnas; j++){
                 std::cout << _datos[i][j] << " | "; 
-
             }
-
             std::cout << std::endl;
-
         }
-
     }
 
     // sobreescribo los valores de la clase MatrizBase: suma
-    MatrizBase<T>* sumar(const MatrizBase<T>& otra) override{
-
-        MatrizBase<T>* matrizResultante = new MatrizBase<T>[_filas][_columnas];
-
-        for(int i = 0; i < _filas; i++){
-
-            for(int j = 0; j< _columnas; j++){
-
-                matrizResultante[i][j] = _datos[i][j] + otra[i][j];
-
+    MatrizBase<T>* sumar(const MatrizBase<T>& otra) const override {
+        MatrizDinamica<T>* matrizResultante = new MatrizDinamica<T>();
+        matrizResultante->cargarValores();
+        
+        for(int i = 0; i < this->_filas; i++){
+            for(int j = 0; j < this->_columnas; j++){
+                matrizResultante->_datos[i][j] = _datos[i][j] + otra.obtenerElemento(i, j);
             }
-
         }
-
+        
         return matrizResultante;
-
-
     }
-
 
     // este es el destructor de la matriz dinamica.
-    ~MatrizBase{
-
-        for(int i = 0; i < _filas; i++){
-
-            delete[] _datos[i];
-
+    ~MatrizDinamica(){
+        std::cout << "Eliminando Matriz Dinamica..." << std::endl;
+        
+        if(_datos != nullptr) {
+            for(int i = 0; i < this->_filas; i++){
+                delete[] _datos[i];
+            }
+            delete[] _datos;
         }
-
-        delete[] _datos;
-
     }
-
 };
 
 template <typename T, int M, int N>
 class MatrizEstatica : public MatrizBase<T> {
 
+private:
     T _datos[M][N];
 
-    void imprimir() override{
+public:
+    // Constructor
+    MatrizEstatica() {
+        this->_filas = M;
+        this->_columnas = N;
+    }
 
-        for(int i = 0; i < _filas; i++){
+    // Método para obtener elemento
+    T obtenerElemento(int i, int j) const override {
+        return _datos[i][j];
+    }
 
-            for(int j = 0; j<_columnas; j++){
-
-                std::cout << "********* MATRIZ ESTATICA ********" << std::endl;
+    void imprimir() const override {
+        std::cout << "********* MATRIZ ESTATICA ********" << std::endl;
+        for(int i = 0; i < this->_filas; i++){
+            for(int j = 0; j< this->_columnas; j++){
                 std::cout << _datos[i][j] << " | ";
-
             }
-
             std::cout << std::endl;
-
         }
-
     }
 
-    MatrizBase<T>* sumar(const MatrizBase<T>& otra) override{
-
-        MatrizBase<T> matrizResultante = new MatrizBase<T>[_filas][_columnas];
-
-        for(int i = 0; i < _filas; i++){
-
-            for(int j = 0; j < _columnas; j++){
-
-                matrizResultante[i][j] = _datos[i][j] + otra[i][j];
-
-            }   
-
-        }
-
-        return matrizResultante;
-
-    }
-
-    void cargarValores() override{
-
+    MatrizBase<T>* sumar(const MatrizBase<T>& otra) const override {
+        MatrizEstatica<T, M, N>* matrizResultante = new MatrizEstatica<T, M, N>();
         
-
+        for(int i = 0; i < this->_filas; i++){
+            for(int j = 0; j < this->_columnas; j++){
+                matrizResultante->_datos[i][j] = _datos[i][j] + otra.obtenerElemento(i, j);
+            }   
+        }
+        
+        return matrizResultante;
     }
 
-
-
+    void cargarValores() override {
+        int contador = 1;
+        
+        for(int i = 0; i < this->_filas; i++){
+            for(int j = 0; j < this->_columnas; j++){
+                _datos[i][j] = contador * 2;
+                contador++;
+            }
+        }
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,125 @@
 #include <iostream>
+#include "clases.h"
+#include <limits>
+
+
+// prototipos
+void menu();
 
 int main() {
+
+    MatrizBase<int>* matrizA = nullptr;
+    MatrizBase<int>* matrizB = nullptr;
+
+    bool continuarEjecucion = true;
+
+    int matricesCreadas = 0;
+
+    int opcion;
+
+    do{
+
+        menu();
+
+        std::cin >> opcion;
+
+        if(std::cin.good()){
+
+            switch (opcion)
+            {
+            case 1:
+
+                if(matricesCreadas >= 2){
+                    std::cout << "\n--- Error: Ya tienes definidas dos matrices. ---" << std::endl;
+                }else{
+                    if(matricesCreadas == 0) {
+                        matrizA = new MatrizDinamica<int>();
+                        matrizA->cargarValores();
+                        std::cout << "\n--- Matriz A dinámica creada exitosamente ---" << std::endl;
+                        matrizA->imprimir();
+                    } else {
+                        matrizB = new MatrizDinamica<int>();
+                        matrizB->cargarValores();
+                        std::cout << "\n--- Matriz B dinámica creada exitosamente ---" << std::endl;
+                        matrizB->imprimir();
+                    }
+                    matricesCreadas++;
+                }
+                break;
+
+            case 2:
+
+                if(matricesCreadas >= 2){
+                    std::cout << "\n--- Error: Ya tienes definidas dos matrices. ---" << std::endl;
+                }else{
+                    if(matricesCreadas == 0) {
+                        matrizA = new MatrizEstatica<int, 5, 5>();
+                        matrizA->cargarValores();
+                        std::cout << "\n--- Matriz A estática creada exitosamente ---" << std::endl;
+                        matrizA->imprimir();
+                    } else {
+                        matrizB = new MatrizEstatica<int, 5, 5>();
+                        matrizB->cargarValores();
+                        std::cout << "\n--- Matriz B estática creada exitosamente ---" << std::endl;
+                        matrizB->imprimir();
+                    }
+                    matricesCreadas++;
+                }
+                break;
+
+            case 3:
+
+                if(matricesCreadas < 2){
+                    std::cout << "\n--- Error: No tienes la cantidad suficiente de matrices para realizar la operación, tienes definidas: " << matricesCreadas << std::endl;
+                } else {
+                    std::cout << "\n--- Realizando suma de matrices ---" << std::endl;
+                    MatrizBase<int>* resultado = matrizA->sumar(*matrizB);
+                    std::cout << "\n--- Resultado de la suma ---" << std::endl;
+                    resultado->imprimir();
+                    delete resultado;
+                }
+                break;
+
+            case 4:
+                std::cout << "\n--- Gracias por utilizar este programa, desarrollado por Mauricio Rodríguez Manzur ---" << std::endl;
+                continuarEjecucion = false;
+                break;
+
+            default:
+                std::cout << "\n--- Error: Ingrese una opción válida ---" << std::endl;
+                break;
+            }
+
+        }
+        else{
+
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            std::cout << "\n--- Error: Ingrese una opción válida ---" << std::endl;
+
+        }
+
+
+    }while(continuarEjecucion);
+
+    // Liberar memoria
+    if(matrizA != nullptr) {
+        delete matrizA;
+    }
+    if(matrizB != nullptr) {
+        delete matrizB;
+    }
+
     return 0;
+}
+
+void menu(){
+
+    std::cout << "\n---- Bienvenido a MathSoft ----"<< std::endl;
+    std::cout << "[1] Crear Matriz Dinamica"        << std::endl;
+    std::cout << "[2] Crear Matriz Estatica"        << std::endl;
+    std::cout << "[3] Suma de Matrices"             << std::endl;
+    std::cout << "[4] Salir de la Aplicación\n\n"   << std::endl;
+
 }
